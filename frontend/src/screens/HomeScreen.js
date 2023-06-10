e33b968fb8f520382a54d64a47bddd08533cff90
loganh1218 <logan991218@gmail.com>
2023-06-10 12:25:22 +0800

    update axios get testing 2

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -32,7 +32,7 @@ function HomeScreen() {
     const fetchData = async () => {
       dispatch({ type: "FETCH_REQUEST" });
       try {
-        const result = await axios.get("https://cipher-ecommerce-atise-frontend.onrender.com/api/products");
+        const result = await axios.get("https://cipher-ecommerce-atise.onrender.com/api/products");
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
         dispatch({ type: "FETCH_FAIL", payload: err.message });
